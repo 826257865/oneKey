fc01e590e11a6e3fda91a6d5b828ce21fcf28a51
dc007 <dc007@zhangyulei.local>
2015-11-02 17:07:14 +0800

    dsadsad

--- SSHkey/ViewController.m
+++ SSHkey/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"123");
+    NSLog(@"32113123");
 }
 
 - (void)didReceiveMemoryWarning {
